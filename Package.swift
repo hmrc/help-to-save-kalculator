c526c36bebf15a46d2e84ae027e7281bf1787877
Christopher O'Brien <4126049+chrisob55@users.noreply.github.com>
2021-02-01 08:23:21 +0000

    feature/HMA-4013 - Moving Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HelpToSaveKalculator",
-            path: "./HelpToSaveKalculator.xcframework"
+            path: "swiftpackage/HelpToSaveKalculator.xcframework"
         ),
     ]
 )
