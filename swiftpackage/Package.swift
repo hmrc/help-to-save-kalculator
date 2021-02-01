e03e77d5b40e6f4b148a4bef7b9bdf7989f8cf9f
Christopher O'Brien <4126049+chrisob55@users.noreply.github.com>
2021-02-01 08:15:31 +0000

    feature/HMA-4013 - Adding Package.swift

---
+++ swiftpackage/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "HelpToSaveKalculator",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "HelpToSaveKalculator",
+            targets: ["HelpToSaveKalculator"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "HelpToSaveKalculator",
+            path: "./HelpToSaveKalculator.xcframework"
+        ),
+    ]
+)
