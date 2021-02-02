af678f3f80c245d34a920665e1b4d1fe6395a60e
Christopher O'Brien <4126049+chrisob55@users.noreply.github.com>
2021-02-02 17:35:33 +0000

    Bump version to 0.5.0 and tagging release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+
+## [0.5.0] - 2021-02-02
 - Added support for `Swift Package Manager`
 
 ## [0.4.0] - 2020-12-08
--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HelpToSaveKalculator",
-            url: "https://github.com/hmrc/help-to-save-kalculator/releases/download/0.4.1/HelpToSaveKalculator.xcframework.zip",
-            checksum: "24f71d23d4586a9b9e67c484984a1bd2b9e6313d4a1fcc7eaa41b4b1ebe52a76"
+            url: "https://github.com/hmrc/help-to-save-kalculator/releases/download/0.5.0/HelpToSaveKalculator.xcframework.zip",
+            checksum: "a6fa980dde3ebd7eccb763804a8ee0cb0aeb6ee31641b5db0b745505cb3b6059"
         ),
     ]
 )
