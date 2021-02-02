a0ecb34ef856bdc242b83493f9aaceee6cd16cd0
Christopher O'Brien <4126049+chrisob55@users.noreply.github.com>
2021-02-02 08:35:44 +0000

    feature/HMA-4013 - fastlane updates

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HelpToSaveKalculator",
-            url: "https://github.com/hmrc/help-to-save-kalculator/releases/download/0.4.1/HelpToSaveKalculator.xcframework.zip",
+            url: "https://github.com/hmrc/help-to-save-kalculator/releases/download/0.4.0/HelpToSaveKalculator.xcframework.zip",
             checksum: "a0ea982423ca636477afe614a13c01d84f02644a18ef5bbb146654684f0ec5a4"
         ),
     ]
