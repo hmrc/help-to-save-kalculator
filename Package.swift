2997d49652b3d015ecc585294330a91b8d6f4179
Christopher O'Brien <4126049+chrisob55@users.noreply.github.com>
2021-02-02 08:36:25 +0000

    Bump version to 0.4.1 and tagging release

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HelpToSaveKalculator",
-            url: "https://github.com/hmrc/help-to-save-kalculator/releases/download/0.4.0/HelpToSaveKalculator.xcframework.zip",
-            checksum: "a0ea982423ca636477afe614a13c01d84f02644a18ef5bbb146654684f0ec5a4"
+            url: "https://github.com/hmrc/help-to-save-kalculator/releases/download/0.4.1/HelpToSaveKalculator.xcframework.zip"
+            checksum: "24f71d23d4586a9b9e67c484984a1bd2b9e6313d4a1fcc7eaa41b4b1ebe52a76"
         ),
     ]
 )
